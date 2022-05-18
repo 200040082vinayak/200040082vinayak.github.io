e121c0bed78a8619efd5b01a66270c4c39d83dd0
Vinayak Mittal <200040082@iitb.ac.in>
2022-05-18 16:40:21 +0530

    connections component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,7 +9,8 @@ import { FeedPageComponent } from './feed-page/feed-page.component';
 import { ProfilePageComponent } from './profile-page/profile-page.component';
 import { HeaderComponent } from './header/header.component';
 
-import { HttpClientModule } from '@angular/common/http'
+import { HttpClientModule } from '@angular/common/http';
+import { ConnectionsComponent } from './connections/connections.component'
 
 @NgModule({
   declarations: [
@@ -18,7 +19,8 @@ import { HttpClientModule } from '@angular/common/http'
     RegistrationPageComponent,
     FeedPageComponent,
     ProfilePageComponent,
-    HeaderComponent
+    HeaderComponent,
+    ConnectionsComponent
   ],
   imports: [
     BrowserModule,
